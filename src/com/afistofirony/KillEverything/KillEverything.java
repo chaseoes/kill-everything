7beb2148701a95bdfe62bce490745b73a9cc502d
afistofirony <afistofirony@me.com>
2013-03-09 22:28:26 -0500

    Challenge accepted

---
+++ src/com/afistofirony/KillEverything/KillEverything.java
@@ -0,0 +1,25 @@
+package com.afistofirony.KillEverything;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginManager;
+
+public class KillEverything extends JavaPlugin{
+    PluginManager EventManager;
+    // This is all pseudo-code, don't take it seriously.
+    public void onEnable(){
+        EventManager.registerEvents(this, this)
+    }
+
+    @EventHandler
+    public boolean onPlayerChatEvent(PlayerChatEvent event){
+        if(event.getMessage().equalsIgnoreCase("kk")){
+            for(Player p : Bukkit.getOnlinePlayers()){
+                 p.setHealth(0);
+                 getServer().broadcastMessage("That phrase is not allowed.")
+            }
+            event.setCancelled(true);
+        }
+        return false;
+    }
+
+}
